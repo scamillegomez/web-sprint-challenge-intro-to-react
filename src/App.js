cce8a12497207c8e7491f104c2d6b3215562cd9b
scgomez <sydneycamille0896@gmail.com>
2023-07-20 11:47:30 -0500

    completed assignment but not passing tests

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [character,setCharacter] = useState([]);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const open = '\u25bc'
+  const close = '\u25b2'
 
+
+  useEffect(()=>{
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res=>{
+      //console.log(res.data.results);
+       setCharacter(res.data.results);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  },[]);
+  // TO DOs - state = set the data ; useEffect to setData = to API data
+  
+  console.log(character);
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {character.map(character=>{
+        return <Character character={character} key={character.id} name={character.name} open={open} close={close}/>
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,58 @@
 // Write your Character component here
+import React, { useState } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+
+const StyledPanel = styled.div`
+    &.panel-content{
+        display:none;
+    }
+    &.toggle-on{
+        display:block;
+    }
+`
+
+const StyledButton = styled.button`
+    &.hide-btn{
+        display:none;
+    }
+`
+
+const Span = styled.span`
+    font-weight: bold;
+`
+
+const Character = props => {
+    const [isExpanded,setExpanded] = useState(false);
+
+    const toggleClass = () => {
+        setExpanded(!isExpanded);
+    };
+
+    return(
+        <div className="panel">
+            <div className="panel-bar">
+                <h3>{props.name}</h3>
+                <div className="panel-buttons">
+                    <StyledButton className={`panel-btn ${isExpanded ? "hide-btn" : ""}`} 
+                        onClick={toggleClass} >
+                            {props.open}
+                    </StyledButton>
+                    <StyledButton className={`panel-btn ${isExpanded ? "" : "hide-btn"}`} 
+                        onClick={toggleClass} >
+                            {props.close}
+                    </StyledButton>
+                </div>
+                <StyledPanel className={`panel-content ${isExpanded ? "toggle-on" : ""}`}>
+                    <p>  
+                        <Span>Birth Year:</Span> {props.character.birth_year}<br />
+                        <Span>Eye Color:</Span> {props.character.eye_color}<br />
+                        <Span>Mass:</Span> {props.character.mass}<br />
+                    </p>
+                </StyledPanel>
+            </div>
+        </div>
+    );
+};
+
+export default Character;
